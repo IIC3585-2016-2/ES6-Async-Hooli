fc01a302c121b336a11eef9e3615396f66a0785f
vdrg <vrdragicevic@gmail.com>
2016-08-07 19:47:17 -0400

    Add demo

---
+++ demo/promises-sync.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const file1 = fs.readFileSync('file1.txt');
+console.log(1);
+
+const file2 = fs.readFileSync('file2.txt');
+console.log(2);
+
+const file3 = fs.readFileSync('file3.txt');
+console.log(3);
